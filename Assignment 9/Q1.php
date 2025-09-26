1d1217a29193cff65446d6fdd09aa2d81fe4054b
Rishi Raj <pothanaboyina.rishi2023@vitstudent.ac.in>
2025-09-26 15:17:13 +0530

    Update Q1.php

--- Assignment 9/Q1.php
+++ Assignment 9/Q1.php
@@ -1,1 +1,27 @@
+<?php
+$students = [
+    "Rahul" => 85,
+    "Priya" => 92,
+    "Arun"  => 78,
+    "Meera" => 95,
+    "Karan" => 88
+];
+
+echo "<h3>All Students & Marks</h3>";
+foreach($students as $name => $marks){
+    echo "$name : $marks<br>";
+}
+
+arsort($students); // Sort by marks descending
+
+echo "<h3>Top 3 Students</h3>";
+echo "<table border='1' cellpadding='5'><tr><th>Name</th><th>Marks</th></tr>";
+$count = 0;
+foreach($students as $name => $marks){
+    if($count == 3) break;
+    echo "<tr><td>$name</td><td>$marks</td></tr>";
+    $count++;
+}
+echo "</table>";
+?>
 
